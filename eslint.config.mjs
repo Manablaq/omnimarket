ab4c9b21c71bfe4391a534825e311fbf4bfe95f3
Manablaq <sundayalbert242@gmail.com>
2026-08-09 03:03:19 +0100

    Restore Next ESLint plugin for Vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
+import next from "@next/eslint-plugin-next";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -20,6 +21,15 @@ const eslintConfig = defineConfig([
   react.configs.flat["jsx-runtime"],
   reactHooks.configs.flat["recommended-latest"],
   jsxA11y.flatConfigs.recommended,
+  {
+    plugins: {
+      "@next/next": next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs["core-web-vitals"].rules,
+    },
+  },
   {
     languageOptions: {
       globals: {
